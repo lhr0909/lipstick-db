609128a6b9e8c8f2037b00d010ddb23c209e3026
Simon Liang <no.lhr0909@gmail.com>
2022-08-03 19:35:20 +0800

    ui pages update

--- ui/pages/en/index.tsx
+++ ui/pages/en/index.tsx
@@ -12,6 +12,22 @@ export default function IndexPage(props: { body: string }) {
     <>
       <Title level={2}>Lipstick DB</Title>
       <Paragraph>Find the right lipstick using AI!</Paragraph>
+      <Paragraph>
+        <img src="https://jina.ai/assets/images/logo.svg" alt="jina" />
+      </Paragraph>
+      <Paragraph>Powered by Jina</Paragraph>
+      <Paragraph>
+        Jina is a framework that empowers anyone to build cross-modal and
+        multi-modal. Visit their{" "}
+        <a
+          href="https://github.com/jina-ai/jina"
+          target="_blank"
+          rel="noreferrer"
+        >
+          repo
+        </a>{" "}
+        and give a star!
+      </Paragraph>
     </>
   );
 }
